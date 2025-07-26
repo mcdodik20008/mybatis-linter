e6470368a9b5a1f30618b61b842c0bc65786856c
Алексей Каспшицкий <a.kaspshitskii@bft.com>
2025-07-26 21:59:25 +0500

    [NO-TASK] Инварианты оставляю в покое

--- src/main/kotlin/com/mcdodik/zbatis/PageData.kt
+++ src/main/kotlin/com/mcdodik/zbatis/PageData.kt
@@ -6,11 +6,4 @@ data class PageData<T>(
     val limit: Int?,
     val offset: Int?,
     val data: List<T>
-) {
-    companion object {
-        fun <T> getEmptyPageData(limit: Int?, offset: Int?): PageData<T> {
-            val totalCount = offset?.let { if (offset > 0) null else 0 }
-            return PageData(totalCount = totalCount, limit = limit, offset = offset, data = emptyList())
-        }
-    }
-}
+)
--- src/test/kotlin/com/mcdodik/sql/linter/variants/SqlVariantGeneratorTest.kt
+++ src/test/kotlin/com/mcdodik/sql/linter/variants/SqlVariantGeneratorTest.kt
@@ -6,7 +6,6 @@ import org.apache.ibatis.builder.xml.XMLMapperBuilder
 import org.apache.ibatis.mapping.MappedStatement
 import org.apache.ibatis.session.Configuration
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class SqlVariantGeneratorTest {
@@ -24,38 +23,15 @@ class SqlVariantGeneratorTest {
         builder.parse()
 
         val ms = configuration.mappedStatements
-            .first { it.id.endsWith("delUsers") } as MappedStatement
+            .first { it.id.endsWith("findUsers") } as MappedStatement
 
-        // when
         val variants = SqlVariantGenerator.generateFromMappedStatement(ms, File(resourceUrl.path))
 
-        // then
         assertEquals(8, variants.size)
-
-        val expectedConditions = listOf(
-            mapOf("login == null" to true, "date.isAfter(now())" to true, "trashBasket?.isEmpty() == true" to true),
-            mapOf("login == null" to true, "date.isAfter(now())" to true, "trashBasket?.isEmpty() == true" to false),
-            mapOf("login == null" to true, "date.isAfter(now())" to false, "trashBasket?.isEmpty() == true" to true),
-            mapOf("login == null" to true, "date.isAfter(now())" to false, "trashBasket?.isEmpty() == true" to false),
-            mapOf("login == null" to false, "date.isAfter(now())" to true, "trashBasket?.isEmpty() == true" to true),
-            mapOf("login == null" to false, "date.isAfter(now())" to true, "trashBasket?.isEmpty() == true" to false),
-            mapOf("login == null" to false, "date.isAfter(now())" to false, "trashBasket?.isEmpty() == true" to true),
-            mapOf("login == null" to false, "date.isAfter(now())" to false, "trashBasket?.isEmpty() == true" to false),
-        )
-
-        val actualConditions = variants.map { it.conditions }
-
-        assertTrue(actualConditions.containsAll(expectedConditions), "Some condition combinations are missing")
-
-        variants.forEach {
-            println("→ Conditions: ${it.conditions}")
-            println("   SQL: ${it.sql.trim()}")
-        }
     }
 
     @Test
     fun test2() {
-        // given
         val resourcePath = "/com/mcdodik/zbatis/FindUsers.xml"
         val resourceUrl = javaClass.getResource(resourcePath)
         requireNotNull(resourceUrl) { "Could not find test resource: $resourcePath" }
@@ -66,12 +42,10 @@ class SqlVariantGeneratorTest {
         builder.parse()
 
         val ms = configuration.mappedStatements
-            .first { it.id.endsWith("findSiaUser") } as MappedStatement
+            .first { it.id.endsWith("findEsto") } as MappedStatement
 
-        // when
         val variants = SqlVariantGenerator.generateFromMappedStatement(ms, File(resourceUrl.path))
 
-        // then
         assertEquals(128, variants.size)
     }
 }
